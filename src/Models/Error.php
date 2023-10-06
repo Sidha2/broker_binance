c7d605e800e90dc1b4dd3fdfe194f3db2dde0046
Jaroslav Bedri <jaroslav.bedri@xolution.sk>
2023-10-06 22:17:19 +0200

    Added props getters

--- src/Models/Error.php
+++ src/Models/Error.php
@@ -6,12 +6,12 @@ use BrokerBinance\Enums\ErrorType;
 
 class Error
 {
-    public string $msg;
-    public ErrorType $errorType;
-    public string $comesFrom;
-    public string $file;
-    public int $line;
-    public int $timestamp;
+    private string $msg;
+    private ErrorType $errorType;
+    private string $comesFrom;
+    private string $file;
+    private int $line;
+    private int $timestamp;
 
     public function __construct(ErrorType $errorType, string $msg, string $comesFrom, string $file, int $line)
     {
@@ -21,5 +21,57 @@ class Error
         $this->file = $file;
         $this->line = $line;
         $this->timestamp = time();
+    }
+
+    /**
+     * Get the value of msg
+     */
+    public function getMsg()
+    {
+        return $this->msg;
+    }
+
+    /**
+     * Get the value of errorType
+     */
+    public function getErrorType()
+    {
+        return $this->errorType;
+    }
+
+    /**
+     * Get the value of comesFrom
+     */
+    public function getComesFrom()
+    {
+        return $this->comesFrom;
+    }
+
+    /**
+     * Set the value of file
+     *
+     * @return  self
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of line
+     */
+    public function getLine()
+    {
+        return $this->line;
+    }
+
+    /**
+     * Get the value of timestamp
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
     }
 }
